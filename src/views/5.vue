2392fc77eeddc53c9554d17a1ea6b497e4125974
Flying <b.noppawat@gmail.com>
2021-05-12 23:12:49 +0700

    Create 5.vue

---
+++ src/views/5.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container class="grey lighten-5" >
+  <v-card class="ma-5" height"1200">
+    <v-row class="mx-0.4">
+      <v-col align="center">
+        <v-card color="red" height="50" width="150">
+          <div class="pa-2 white--text">
+            ประวัติการซื้อ-ขาย
+          </div></v-card
+        >
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card contain height="60" class="red lighten-3">
+          <v-col cols="1">
+            ชื่อ
+          </v-col>
+          <v-col cols="1">
+            <v-list-item-avatar
+              ><v-img
+                src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
+              >
+              </v-img>
+            </v-list-item-avatar>
+          </v-col>
+        </v-card>
+      </v-col>
+
+      <v-col cols="3">
+        <v-card contain height="60" class="red lighten-3">
+          5
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+        <v-col cols="6">
+        <v-card contain height="60" class="red lighten-3">
+            ชื่อ
+            </v-card>
+        <v-col cols="1">
+        <v-list-item-avatar
+        ><v-img
+                src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
+              >
+              </v-img>
+            </v-list-item-avatar>
+            
+        </v-col>
+        </v-col>
+    </v-row>
+  </v-card>
+  </v-container>
+</template>
