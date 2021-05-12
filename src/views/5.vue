e407b80a9f4bfeaa77c187aa2c45a321ec77350a
Flying <b.noppawat@gmail.com>
2021-05-13 00:59:08 +0700

    Update 5.vue

--- src/views/5.vue
+++ src/views/5.vue
@@ -5,12 +5,29 @@
         <h1>ประวัติการเช่า</h1>
       </v-card-title>
 
-      <v-row class="ma-5">
-        <v-card contain height="70" class="red lighten-3">
+      <v-card
+        class="d-flex flex-row mb-6"
+        :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
+        flat
+        tile
+      >
+        <v-card
+          contain
+          height="70"
+          class="red 
+        lighten-3"
+          rounded="0"
+        >
           <v-card-title class="pa-5">19/09/64</v-card-title>
         </v-card>
 
-        <v-card contain height="70" class="red lighten-3">
+        <v-card
+          contain
+          height="70"
+          class="red 
+        lighten-3"
+          rounded="0"
+        >
           <v-card-title class="pa-3">
             <v-list-item>
               <v-list-item-avatar size="40">
@@ -25,13 +42,25 @@
           </v-card-title>
         </v-card>
 
-        <v-card contain height="70" class="red lighten-3">
+        <v-card
+          contain
+          height="70"
+          class="red 
+        lighten-3"
+          rounded="0"
+        >
           <v-card-title class="pa-6">
             50000
           </v-card-title>
         </v-card>
 
-        <v-card contain height="70" class="red lighten-3">
+        <v-card
+          contain
+          height="70"
+          class="red 
+          lighten-3"
+          rounded="0"
+        >
           <v-card-title class="pa-6">
             50000
           </v-card-title>
@@ -39,22 +68,29 @@
 
         <v-dialog transition="dialog-top-transition" max-width="600">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn depressed color="primary" height="70" width="200"
-              >หลักฐานการโอนเงิน</v-btn
+            <v-btn
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+              width="150"
+              height="50"
+              class="ma-2 "
+              >ใบเสร็จ</v-btn
             >
           </template>
           <template v-slot:default="dialog">
             <v-card>
-              <v-img
-                src="https://www.microsuck.net/wp-content/uploads/2019/09/1-592x1024.png"
-              ></v-img>
+              <v-toolbar color="primary" dark>Opening from the top</v-toolbar>
+              <v-card-text>
+                <div class="text-h2 pa-12">Hello world!</div>
+              </v-card-text>
               <v-card-actions class="justify-end">
                 <v-btn text @click="dialog.value = false">Close</v-btn>
               </v-card-actions>
             </v-card>
           </template>
         </v-dialog>
-      </v-row>
+      </v-card>
 
       <v-row class="ma-5">
         <v-col cols="2">
@@ -80,18 +116,41 @@
         </v-col>
         <v-col cols="2">
           <v-card contain height="70" class="red lighten-3">
-            <v-card-title class="pa-5">5000 Bath</v-card-title>
+            <v-card-title class="pa-5">5 ชั่วโมง</v-card-title>
           </v-card>
         </v-col>
         <v-col col="2">
           <v-card contain height="70" class="red lighten-3">
-            <v-card-title class="pa-5">5 ชั่วโมง</v-card-title>
+            <v-card-title class="pa-5">5000</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="2" align="center">
-          <v-btn depressed color="primary" height="70" width="200">
-            ใบเสร็จ
-          </v-btn>
+          <v-dialog
+            transition="dialog-top-transition"
+            width="1000"
+            height="1000"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                v-bind="attrs"
+                v-on="on"
+                width="200"
+                height="70"
+                >ใบเสร็จ</v-btn
+              >
+            </template>
+            <template v-slot:default="dialog">
+              <v-card>
+                <v-img
+                  src="https://www.microsuck.net/wp-content/uploads/2019/09/1-592x1024.png"
+                ></v-img>
+                <v-card-actions class="justify-end">
+                  <v-btn @click="dialog.value = false" color="er">Close</v-btn>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
         </v-col>
       </v-row>
     </v-card>
