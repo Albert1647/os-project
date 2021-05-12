77de9cbf3946f8c7234a35ef5ce0674700f62b2c
Natthadecha Kijsamrej <41380751+Albert1647@users.noreply.github.com>
2021-05-12 22:56:26 +0700

    Update page2
    by z

--- src/views/2.vue
+++ src/views/2.vue
@@ -1,26 +1,33 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="12" sm="4">
-                <v-card max-width="500">
+        <v-card 
+        height="1200"
+        elevation="0"
+        >
+        <v-card-title class="display-1" >ข้อมูลส่วนตัว</v-card-title>
+            <v-row class="mb-16">
+            <v-col cols="12" sm="4" class="pa-10">
+                <v-card max-width="500" max-height="500">
                     <v-img
                         src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
-                        height="400px"
+                        width="500" 
+                        height="500"
                     ></v-img>
                 </v-card>
             </v-col>
-            <v-col>
-                <v-card max-width="700">
+            <v-col class="pt-10">
+                <v-card max-width="700"  >
                     <v-card-title>
                         Pekora
-          </v-card-title>
+                    </v-card-title>
                 </v-card>
-                <v-card class="my-4" width="700" height="320">
-                    <v-card-subtitle class="display-1">
-                        สวัสดีครับเราเคยรู้จักกันหรือเปล่า
-          </v-card-subtitle>
+                <v-card class="my-4" width="700" height="600">
+                    <v-card-text>
+                        สวัสดีจ้า เจ้าพวกแครอททั้งหลาย สวัสดีจ้า เจ้าพวกแครอททั้งหลายสวัสดีจ้า เจ้าพวกแครอททั้งหลายสวัสดีจ้า เจ้าพวกแครอททั้งหลายสวัสดีจ้า เจ้าพวกแครอททั้งหลาย
+                    </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
+        </v-card>
     </v-container>
 </template>
