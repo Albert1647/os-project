d2086acc2fb76377aa23d7434b14cccb99d86998
Flying <b.noppawat@gmail.com>
2021-05-12 19:54:41 +0700

    Update 2.vue

--- src/views/2.vue
+++ src/views/2.vue
@@ -1,73 +1,40 @@
-<div>
+<template>
+  <v-container>
     <template>
-        <v-container>
-            <v-col
+      <v-row align-self="start">
+        <v-col cols="20" align-self="center">
+          <v-col cols="20" align-self="center">
+            <v-card class="" max-width="500">
+              <v-card-title>
+                Pekora
+              </v-card-title>
+            </v-card>
+          </v-col>
+        </v-col>
+      </v-row>
+    </template>
+    <v-col cols="12" sm="4">
+      <v-card class="mx-auto" max-width="344">
+        <v-img
+          src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
+          height="400px"
+        ></v-img>
+      </v-card>
+    </v-col>
+  </v-container>
+</template>
 
-                cols="12"
-                sm="4"
-            >
-                <v-card
-                    class="mx-auto"
-                    max-width="344"
-                >
-                    <v-img
-                        src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
-                        height="400px"
-                    ></v-img>
-                </v-card>
-            </v-col>
-
-            <template>
-                <v-row
-
-                    align-self="start"
-                >
-
-                    <v-col
-                        cols="20"
-                        align-self="center"
-                    >
-                        <v-col
-                            cols="20"
-                            align-self="center"
-                        >
-                            <v-card
-                                class=""
-                                max-width="500"
-
-
-                            >
-                                <v-card-title>
-
-                                    Pekora
-
-    </v-card-title>
-                            </v-card>
-                        </v-col>
-                    </v-col>
-                </v-row>
-            </template>
-
-        </v-container>
-
-    </template >
-    <script>
-        export default {
-            data: () => ({
-            show: false,
-    }),
-  }
-
+<script>
+export default {
+  data: () => ({
+    show: false,
+  }),
+};
 </script>
-    <script>
-        export default {
-            data: () => ({
-            alignments: [
-        'start',
-        'center',
-        'end',
-      ],
-    }),
-  }
+<script>
+export default {
+  data: () => ({
+    alignments: ["start", "center", "end"],
+  }),
+};
 </script>
-</div>
