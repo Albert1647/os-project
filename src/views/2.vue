53ee6686c4631800603a08c475545bf04d020c4d
Shikino-code <billso2000@gmail.com>
2021-05-12 20:46:30 +0700

    Finish2vue

--- src/views/2.vue
+++ src/views/2.vue
@@ -1,38 +1,45 @@
 <template>
   <v-container>
-    <template>
-      <v-row align-self="start">
-        <v-col cols="20" align-self="center">
-          <v-col cols="20" align-self="center">
-            <v-card class="" max-width="500">
-              <v-card-title>
-                Pekora
-              </v-card-title>
-            </v-card>
-          </v-col>
-        </v-col>
-      </v-row>
-    </template>
-    <v-col cols="12" sm="4">
-      <v-card class="mx-auto" max-width="344">
-        <v-img
-          src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
-          height="400px"
-        ></v-img>
-      </v-card>
-    </v-col>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <v-card  
+            max-width="500">
+          <v-img
+            src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
+            height="400px"
+          ></v-img>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card  max-width="700">
+          <v-card-title>
+                        Pekora
+          </v-card-title>
+        </v-card>
+        <v-card 
+            class="my-4"
+            width="700"
+            height="320"
+        >
+      <v-card-subtitle>
+                        สวัสดีครับเราเคยรู้จักกันหรือเปล่า
+      </v-card-subtitle>
+                    
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
+        export default {
   data: () => ({
     show: false,
   }),
 };
 </script>
 <script>
-export default {
+        export default {
   data: () => ({
     alignments: ["start", "center", "end"],
   }),
