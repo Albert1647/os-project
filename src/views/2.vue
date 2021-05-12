0c3801e43185e2eb308c466c282dd0056c20a4c5
Shikino-code <billso2000@gmail.com>
2021-05-12 20:56:38 +0700

    Final2Finish

--- src/views/2.vue
+++ src/views/2.vue
@@ -2,8 +2,7 @@
     <v-container>
         <v-row>
             <v-col cols="12" sm="4">
-                <v-card
-                    max-width="500">
+                <v-card max-width="500">
                     <v-img
                         src="https://www.online-station.net/wp-content/uploads/2020/12_1/05-1200x630-4.jpg"
                         height="400px"
@@ -16,21 +15,12 @@
                         Pekora
           </v-card-title>
                 </v-card>
-                <v-card
-                    class="my-4"
-                    width="700"
-                    height="320"
-                >
-                    <v-card-subtitle
-                        class="display-1"
-
-                    >
+                <v-card class="my-4" width="700" height="320">
+                    <v-card-subtitle class="display-1">
                         สวัสดีครับเราเคยรู้จักกันหรือเปล่า
-      </v-card-subtitle>
-
+          </v-card-subtitle>
                 </v-card>
             </v-col>
         </v-row>
     </v-container>
 </template>
-
